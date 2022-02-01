334f6d16dd8aa91f0649249a4caf39c8c85f82ae
Steven Schoebinger <Steven.schoebinger@churchofjesuschrist.org>
2022-02-01 16:07:22 -0700

    blurring background without children

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import TopBar from './TopBar'
-import Home from "./pages/Home.js"
+import "./App.css";
+import TopBar from "./TopBar";
+import Home from "./pages/Home.js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <TopBar/>
-      <div className='Content'> 
-        <Home/>
+      <TopBar id='top'/>
+      <div className="Content">
+        <Home />
       </div>
     </div>
   );
-}
+};
 
 export default App;
