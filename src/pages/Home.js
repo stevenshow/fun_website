aff0d15570bfb143dfec7997b8b680b273552682
Steven Schoebinger <Steven.schoebinger@churchofjesuschrist.org>
2022-02-01 16:07:36 -0700

    Creating cards to have content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,41 @@
-import TopBar from '../TopBar.js'
+import "./Home.css";
+import TopBar from "../TopBar.js";
+import rasp from "../img/raspberrypi.png";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <h1>Home Page Reached!</h1>
+  return (
+    <div className="home">
+      <h1>Hi, I am Steven Schoebinger</h1>
+      <div className="cardHolder">
+        <div className="card">
+          <img src={rasp} alt="raspberrypi" />
+          <div className="container">
+            <h4>
+              <b>Raspberry Pi</b>
+            </h4>
+            <p>Model 4</p>
+          </div>
         </div>
-    )
-}
+        <div class="card">
+          <img src={rasp} alt="raspberrypi" />
+          <div className="container">
+            <h4>
+              <b>Raspberry Pi</b>
+            </h4>
+            <p>Model 4</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={rasp} alt="raspberrypi" />
+          <div className="container">
+            <h4>
+              <b>Raspberry Pi</b>
+            </h4>
+            <p>Model 4</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Home;
