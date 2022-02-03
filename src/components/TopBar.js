190e45f50b72b0a7809d57429e7d19a798a65c42
Steven Schoebinger <Steven.schoebinger@churchofjesuschrist.org>
2022-02-02 19:20:31 -0700

    changed topbar to be terminal style

---
+++ src/components/TopBar.js
@@ -0,0 +1,31 @@
+import "./TopBar.css";
+
+//TODO Find out how to make the top bar "sticky"
+
+const TopBar = () => {
+  return (
+    <div className="header">
+    <p>
+      <span className="green-highlight">steven@schoebinger</span>:
+      <span className="blue-highlight">~</span>$
+      <span className="green-highlight">echo </span>Welcome to my website!
+      <span className="cursor">.</span>
+    </p>
+  </div>
+  );
+};
+
+export default TopBar;
+    
+// <div class="topnav">
+    //   <a className="home-nav" href="home">
+    //     Home
+    //   </a>
+    //   <a className="projects-nav" href="projects">
+    //     Projects
+    //   </a>
+    //   <a className="contact-nav" href="contact">
+    //     Contact
+    //   </a>
+    //   {/* <a href="about">About</a> */}
+    // </div>
