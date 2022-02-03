d71b74ad844462737eceaf1b80d142025b3b5ada
Steven Schoebinger <Steven.schoebinger@churchofjesuschrist.org>
2022-02-02 19:20:47 -0700

    removing topbar from App and placing it in the page itself

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import TopBar from "./TopBar";
 import Home from "./pages/Home.js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <TopBar id='top'/>
       <div className="Content">
         <Home />
       </div>
