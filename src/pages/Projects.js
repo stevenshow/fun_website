537ac4731f0549208a2de3cf1a1a39eb56505af1
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-03-06 22:20:05 -0700

    basic projects page

---
+++ src/pages/Projects.js
@@ -0,0 +1,12 @@
+import "./Projects.css";
+import TopBar from "../components/TopBar";
+
+const Projects = () => {
+    return (
+        <div className="projects">
+            <TopBar />
+        </div>
+    );
+};
+
+export default Projects;
