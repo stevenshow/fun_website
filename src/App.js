39e0497d50c51466a9f94fdefc967bcef660d3a8
Steven Schoebinger <Steven.schoebinger@churchofjesuschrist.org>
2022-03-17 00:08:39 -0600

    logic to handle theme changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
+// import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css";
 import Home from "./pages/Home.js";
-import TopBar from "./components/TopBar"
-import Projects from "./pages/Projects"
+import TopBar from "./components/TopBar";
+import Projects from "./pages/Projects";
 // import Contact from "./pages/Contact.js"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function changeTheme() {
-  document.getElementById('root').classList.toggle('green');
-  document.getElementById('root').classList.toggle('pink');
-};
+function changeGreen() {
+  // document.getElementById("root").classList.toggle("green");
+  if (document.getElementById("root").classList.contains("pink")) {
+    document.getElementById("root").classList.toggle("pink");
+  }
+
+  if (document.getElementById("root").classList.contains("lightblue")) {
+    document.getElementById("root").classList.toggle("lightblue");
+  }
+}
+
+function changePink() {
+  if (document.getElementById("root").classList.contains("lightblue")) {
+    document.getElementById("root").classList.toggle("lightblue");
+  }
+
+  document.getElementById("root").classList.toggle("pink");
+}
+
+function changeLightBlue() {
+  if (document.getElementById("root").classList.contains("pink")) {
+    document.getElementById("root").classList.toggle("pink");
+  }
+
+  document.getElementById("root").classList.toggle("lightblue");
+}
 
 const App = () => {
   return (
-   <Router>
-      <TopBar themeChange={changeTheme}/>
+    <Router>
+      <TopBar
+        changeGreen={changeGreen}
+        changePink={changePink}
+        changeLightBlue={changeLightBlue}
+      />
       <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<Projects />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Projects />} />
       </Routes>
     </Router>
   );
