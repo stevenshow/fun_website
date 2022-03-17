f83c4bac5231c66225d415ca5142fbc9ed2354dc
Steven Schoebinger <Steven.schoebinger@churchofjesuschrist.org>
2022-03-17 00:08:58 -0600

    button changes themes

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,20 +1,6 @@
 import "./TopBar.css";
 import { Link } from "react-router-dom";
 
-
-{/* <Dropdown>
-  <Dropdown.Toggle variant="success" id="dropdown-basic">
-    Dropdown Button
-  </Dropdown.Toggle>
-
-  <Dropdown.Menu>
-    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-  </Dropdown.Menu>
-</Dropdown> */}
-
-
 const TopBar = (props) => {
   return (
     <div className="header">
@@ -28,11 +14,25 @@ const TopBar = (props) => {
         <span className="blue-highlight">~</span>$
         <span className="cursor"> _</span>
       </div>
-        <button onClick={props.themeChange}>Click Me</button>
+      {/* <button onClick={props.themeChange}>Click Me</button> */}
+      <div className="dropdown">
+        <button className="dropbtn">Colors</button>
+        <div className="dropdown-content">
+          <button className="green-button" onClick={props.changeGreen}>Green</button>
+          <button className="pink-button" onClick={props.changePink}>Pink</button>
+          <button className="light-blue-button" onClick={props.changeLightBlue}>Blue</button>
+        </div>
+      </div>
       <div className="links-right">
-        <Link className="link" to="/">Home</Link>
-        <Link className="link projects-nav" to="/projects">Projects</Link>
-        <Link className="link" to="/contact">Contact</Link>
+        <Link className="link" to="/">
+          Home
+        </Link>
+        <Link className="link projects-nav" to="/projects">
+          Projects
+        </Link>
+        <Link className="link" to="/contact">
+          Contact
+        </Link>
       </div>
     </div>
   );
