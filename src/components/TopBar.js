7aa64eb6549e6b038ae0ecf7c6d1caa47b7d36e5
Steven Schoebinger <Steven.schoebinger@churchofjesuschrist.org>
2022-03-18 15:10:03 -0600

    got command line color changing to work

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,25 +1,83 @@
-import "./TopBar.css";
+import "./TopBar.scss";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+const commands = require("../utils/commands");
+// console.log(commands)
+
+const changeTheme = (color) => {
+  let element = document.getElementById("root");
+  element.classList.remove(...element.classList);
+  document.getElementById("root").classList.toggle(color);
+}
+
+const executeCommand = (userCommand, userParameters) => {
+  console.log('command:', userCommand)
+  console.log('parameters:', ...userParameters)
+  commands.forEach(({ command, execute, parameters }) => {
+
+    if (execute !== undefined && userCommand === command) {
+      switch (execute) {
+        case "theme":
+          changeTheme(...userParameters)
+          break;
+        default:
+          break;
+      }
+    }
+  });
+}
+
 
 function Command() {
   const [input, setInput] = useState(""); // '' is the initial state value
+  let valid;
+  let command;
+
+  const clearInput = () => {
+    setInput("");
+    document.querySelector(".command").classList.remove("red");
+  }
 
   const handle = () => {
-    console.log(input);
     // Going to probably need regex to test for valid command and arg
-    if (input.toLocaleLowerCase() === "color") {
-      console.log('changed color')
+    console.log(valid)
+    if (valid) {
+
+      // execute command
+      let parameters = input.split(" ").slice(1);
+      executeCommand(command[0], parameters)
+      // clear input
+      clearInput();
     }
-    setInput("");
+    else {
+      // give error message
+      setInput(`try typing 'help'`)
+      // clear input
+      setTimeout(clearInput, 1000)
+    }
   };
+// setting the state seems to not update as soon as keeping the valid variable does
+// figure out either how to keep valid usable in handle() or update state fast
+  const checkCommand = () => {
+    command = input.split(" ");
+    valid = commands[0].includes(command[0]) || command[0] === "";
+    if (valid) {
+      document.querySelector(".command").classList.add("valid");
+      document.querySelector(".command").classList.remove("invalid");
+    }
+
+    else {
+      document.querySelector(".command").classList.add("invalid");
+    }
+  }
 
   return (
     <input
       autoFocus
-      className="command"
+      className={`command valid`}
       onInput={(e) => setInput(e.target.value)}
       onKeyDown={(e) => e.key === "Enter" && handle()}
+      onKeyUp={() => checkCommand()}
       type="text"
       value={input}
     />
