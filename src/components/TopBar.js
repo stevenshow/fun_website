cf065d5f773ca5be4ddbc45f0f0c689157498469
Steven Schoebinger <Steven.schoebinger@churchofjesuschrist.org>
2022-03-18 01:06:39 -0600

    added component to gather command and do simple process

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,5 +1,30 @@
 import "./TopBar.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
+function Command() {
+  const [input, setInput] = useState(""); // '' is the initial state value
+
+  const handle = () => {
+    console.log(input);
+    // Going to probably need regex to test for valid command and arg
+    if (input.toLocaleLowerCase() === "color") {
+      console.log('changed color')
+    }
+    setInput("");
+  };
+
+  return (
+    <input
+      autoFocus
+      className="command"
+      onInput={(e) => setInput(e.target.value)}
+      onKeyDown={(e) => e.key === "Enter" && handle()}
+      type="text"
+      value={input}
+    />
+  );
+}
 
 const TopBar = (props) => {
   return (
@@ -11,16 +36,32 @@ const TopBar = (props) => {
         <span className="blue-highlight">$NAME\'s</span> website!
         <div>Welcome to Steven's website!</div>
         <span className="green-highlight">steven@schoebinger</span>:
-        <span className="blue-highlight">~</span>$
-        <span className="cursor"> _</span>
+        <span className="blue-highlight">~</span>${/* <Enter /> */}
+        <Command />
+        {/* <span className="cursor"> _</span> */}
       </div>
       {/* <button onClick={props.themeChange}>Click Me</button> */}
       <div className="dropdown">
         <div className="dropbtn">Colors</div>
         <div className="dropdown-content">
-          <button className="green-button" onClick={() => props.changeTheme('green')}>Green</button>
-          <button className="pink-button" onClick={() => props.changeTheme('pink')}>Pink</button>
-          <button className="light-blue-button" onClick={() => props.changeTheme('lightblue')}>Blue</button>
+          <button
+            className="green-button"
+            onClick={() => props.changeTheme("green")}
+          >
+            Green
+          </button>
+          <button
+            className="pink-button"
+            onClick={() => props.changeTheme("pink")}
+          >
+            Pink
+          </button>
+          <button
+            className="light-blue-button"
+            onClick={() => props.changeTheme("lightblue")}
+          >
+            Blue
+          </button>
         </div>
       </div>
       <div className="links-right">
