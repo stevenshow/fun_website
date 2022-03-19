57d82abff757d6b9fdb0d1be0ee9477c50f19ee2
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-03-19 14:47:43 -0600

    Add 6th card to be even

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 // import 'bootstrap/dist/css/bootstrap.css';
-import "./App.css";
-import Home from "./pages/Home.js";
-import TopBar from "./components/TopBar";
-import Projects from "./pages/Projects";
+import './App.css'
+import Home from './pages/Home.js'
+import TopBar from './components/TopBar'
+import Projects from './pages/Projects'
 // import Contact from "./pages/Contact.js"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function changeTheme(color) {
-  let element = document.getElementById("root");
-  element.classList.remove(...element.classList);
-  document.getElementById("root").classList.toggle(color);
+  let element = document.getElementById('root')
+  element.classList.remove(...element.classList)
+  document.getElementById('root').classList.toggle(color)
 }
 
 const App = () => {
   return (
     <Router>
-      <TopBar
-        changeTheme={changeTheme}
-      />
+      <TopBar changeTheme={changeTheme} />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
       </Routes>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
