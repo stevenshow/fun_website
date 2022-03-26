4054ca5f177fc6b29b5b63013f9338ce3bf7ffa6
Steven Schoebinger <Steven.schoebinger@churchofjesuschrist.org>
2022-03-25 23:28:16 -0600

    attempting to get dialog to act as modal for help command

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,126 +1,119 @@
-import './TopBar.scss'
-import Prompt from './Prompt'
-import Modal from 'react-modal'
-import { Link } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import { changeTheme, executeCommand } from '../utils/execution'
-const commands = require('../utils/commands')
+import "./TopBar.scss";
+import Prompt from "./Prompt";
+import Modal from "react-modal";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { changeTheme, executeCommand } from "../utils/execution";
+const commands = require("../utils/commands");
+
+const closeModal = () => {
+  const modal = document.querySelector(".help-modal");
+  console.log('close', modal)
+  modal.close();
+};
+const openModal = () => {
+  const modal = document.querySelector(".help-modal");
+  console.log('open', modal)
+  modal.showModal();
+};
 
 function Command() {
-  const [input, setInput] = useState('')
-  let valid
-  let command
+  const [input, setInput] = useState("");
+  let valid;
+  let command;
 
   const clearInput = () => {
-    setInput('')
-    document.querySelector('.currentcommand').classList.remove('invalid')
-  }
+    setInput("");
+    document.querySelector(".currentcommand").classList.remove("invalid");
+  };
 
   const handle = () => {
     // Going to probably need regex to test for valid command and arg
     if (valid) {
+      if (command[0] === "help") {
+        const modal = document.querySelector(".help-modal");
+        console.log('command',modal)
+        modal.showModal();
+        // openModal()
+        clearInput()
+        return
+      }
       // execute command
-      let parameters = input.split(' ').slice(1)
-      executeCommand(command[0], ...parameters)
-      // clear input
-      clearInput()
+      let parameters = input.split(" ").slice(1);
+      executeCommand(command[0], ...parameters);
+      clearInput();
     } else {
       // give error message
-      console.log(valid)
-      setInput(`try typing 'help'`)
+      console.log(valid);
+      setInput(`try typing 'help'`);
       // clear input
-      setTimeout(clearInput, 1000)
+      setTimeout(clearInput, 1000);
     }
-  }
+  };
 
   const checkCommand = () => {
-    command = input.split(' ')
-    valid = commands['Valid'].includes(command[0]) || command[0] === ''
+    command = input.split(" ");
+    valid = commands["Valid"].includes(command[0]) || command[0] === "";
     if (valid) {
-      document.querySelector('.currentcommand').classList.add('valid')
-      document.querySelector('.currentcommand').classList.remove('invalid')
+      document.querySelector(".currentcommand").classList.add("valid");
+      document.querySelector(".currentcommand").classList.remove("invalid");
     } else {
-      document.querySelector('.currentcommand').classList.add('invalid')
+      document.querySelector(".currentcommand").classList.add("invalid");
     }
-  }
+  };
 
   return (
     <input
       autoFocus
       className={`currentcommand valid`}
       onInput={(e) => setInput(e.target.value)}
-      onKeyDown={(e) => e.key === 'Enter' && handle()}
+      onKeyDown={(e) => e.key === "Enter" && handle()}
       onKeyUp={() => checkCommand()}
-      spellCheck='false'
-      type='text'
+      spellCheck="false"
+      type="text"
       value={input}
     />
-  )
+  );
 }
-// Need to figure out how to get setIsOpen to change to true when the execute command is run successfully in <Command>
-const TopBar = (props) => {
-  Modal.setAppElement('#root');
-  const [modalIsOpen, setIsOpen] = useState(false)
-  const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-    },
-  };
-  
-  function openModal() {
-    setIsOpen(true)
-  }
-  
-  function closeModal() {
-    setIsOpen(false)
-  }
-  // return (
 
-  // )
+const TopBar = (props) => {
+
   return (
-    // <HelpModal>
-    <div className='header'>
-          <Modal
-    ariaHideApp={false}
-      className='help-modal'
-      isOpen={modalIsOpen}
-      // onAfterOpen={afterOpenModal}
-      onRequestClose={closeModal}
-      style={customStyles}
-      contentLabel='Help Modal'
-    > <button onClick={closeModal}>close</button></Modal>
+    <div className="header">
       <div>
-        <span className='green-highlight'>steven@schoebinger</span>:
-        <span className='blue-highlight'>~/</span>$
-        <span className='green-highlight'> echo </span>Welcome to{' '}
-        <span className='blue-highlight'>$NAME\'s</span> website!
+        <span className="green-highlight">steven@schoebinger</span>:
+        <span className="blue-highlight">~/</span>$
+        <span className="green-highlight"> echo </span>Welcome to{" "}
+        <span className="blue-highlight">$NAME\'s</span> website!
         <div>Welcome to Steven's website!</div>
-        <span className='green-highlight'>steven@schoebinger</span>:
-        <span className='blue-highlight'>~/</span>
-        <span className='directory blue-highlight'></span>$
+        <span className="green-highlight">steven@schoebinger</span>:
+        <span className="blue-highlight">~/</span>
+        <span className="directory blue-highlight"></span>$
         {/* <br></br>
         <span className='test'>test</span> */}
         {/* <Prompt prevCommand="color orange" classes="command valid"/> */}
         <Command />
+        <button onClick={openModal} className="show-modal">show</button>
+        <dialog className="help-modal">
+          <button onClick={closeModal} className="close-modal">
+            x
+          </button>
+          Hello
+        </dialog>
       </div>
-      <div className='links-right'>
-        <Link id='home' className='link' to='/'>
+      <div className="links-right">
+        <Link id="home" className="link" to="/">
           Home
         </Link>
-        <Link id='projects' className='link projects-nav' to='/projects'>
+        <Link id="projects" className="link projects-nav" to="/projects">
           Projects
         </Link>
-        <Link id='contact' className='link' to='/contact'>
+        <Link id="contact" className="link" to="/contact">
           Contact
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopBar
+export default TopBar;
