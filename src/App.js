1284fc3a7710233929f0bd74fe0d91b359aa2dbb
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-03-26 02:21:53 -0600

    cleaned up old modal

--- src/App.js
+++ src/App.js
@@ -5,47 +5,11 @@ import TopBar from './components/TopBar'
 import Projects from './pages/Projects'
 // import Contact from "./pages/Contact.js"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Modal from 'react-modal'
-import { useState } from 'react'
 
 function changeTheme(color) {
   let element = document.getElementById('root')
   element.classList.remove(...element.classList)
   document.getElementById('root').classList.toggle(color)
-}
-
-function HelpModal() {
-  Modal.setAppElement('#root')
-  const [modalIsOpen, setIsOpen] = useState(false)
-  const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-    },
-  }
-
-  function openModal() {
-    setIsOpen(true)
-  }
-
-  function closeModal() {
-    setIsOpen(false)
-  }
-  return (
-    <Modal
-      ariaHideApp={false}
-      className='help-modal'
-      isOpen={modalIsOpen}
-      // onAfterOpen={afterOpenModal}
-      onRequestClose={closeModal}
-      // style={customStyles}
-      contentLabel='Help Modal'
-    />
-  )
 }
 
 const App = () => {
