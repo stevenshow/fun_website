002af97eb5b91d6ddea1b427683dee8c7b8829ae
Steven Schoebinger <Steven.schoebinger@churchofjesuschrist.org>
2022-03-28 22:43:04 -0600

    nav buttons now change command line directory

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,38 +1,57 @@
-import './TopBar.scss'
-import Command from './Command'
-import { Link } from 'react-router-dom'
+import "./TopBar.scss";
+import Command from "./Command";
+import { Link } from "react-router-dom";
 
 const TopBar = (props) => {
+  const changeDirectory = (pageName) => {
+    let dir = pageName;
+    document.querySelector(".directory").textContent = dir;
+  };
+
   return (
-    <div className='header'>
+    <div className="header">
       <div>
-          <span className='green-highlight'>steven@schoebinger</span>:
-          <span className='blue-highlight'>~/</span>$
-          <span className='green-highlight'> echo </span>
-          {/* Welcome to{' '} */}
-          <span className='blue-highlight'>$WELCOME</span>
-        <div>Welcome to Steven's website! Type '<span className='green-highlight'>help</span>' for list of commands.</div>
-        <span className='green-highlight'>steven@schoebinger</span>:
-        <span className='blue-highlight'>~/</span>
-        <span className='directory blue-highlight'></span>$
+        <span className="green-highlight">steven@schoebinger</span>:
+        <span className="blue-highlight">~/</span>$
+        <span className="green-highlight"> echo </span>
+        {/* Welcome to{' '} */}
+        <span className="blue-highlight">$WELCOME</span>
+        <div>
+          Welcome to Steven's website! Type '
+          <span className="green-highlight">help</span>' for list of commands.
+        </div>
+        <span className="green-highlight">steven@schoebinger</span>:
+        <span className="blue-highlight">~/</span>
+        <span className="directory blue-highlight"></span>$
         {/* <br></br>
         <span className='test'>test</span> */}
         {/* <Prompt prevCommand="color orange" classes="command valid"/> */}
         <Command />
       </div>
-      <div className='links-right'>
-        <Link id='home' className='link' to='/'>
+      <div className="links-right">
+        <Link
+          onClick={() => changeDirectory("")}
+          id="home"
+          className="link"
+          to="/"
+        >
           Home
         </Link>
-        <Link id='projects' className='link projects-nav' to='/projects'>
+        <Link
+        onClick={() => changeDirectory("projects")}
+        id="projects"
+        className="link projects-nav"
+        to="/projects">
           Projects
         </Link>
-        <Link id='contact' className='link' to='/contact'>
+        <Link
+        onClick={() => changeDirectory("contact")}
+        id="contact" className="link" to="/contact">
           Contact
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopBar
+export default TopBar;
