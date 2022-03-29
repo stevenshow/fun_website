1ac9c6fd44b0ee2b917ef33945b94230d457645b
Steven Schoebinger <Steven.schoebinger@churchofjesuschrist.org>
2022-03-28 21:48:29 -0600

    adding real content

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,34 +1,44 @@
 import "./Projects.css";
-import catfact from "../img/cat-fact-email.png"
 
-const projects =  [
-    {
-        name: "Cat Fact Generator",
-        alt: "cat fact email screenshot",
-        image: catfact,
-        id: 1,
-        language: "Language: Python",
-        difficulty: "",
-        content: `Linux is the best-known and most-used open source operating system. As an operating system, Linux is software that sits underneath all of the other software on a computer, receiving requests from those programs and relaying these requests to the computer’s hardware.`,
-      },
-]
+const projects = [
+  {
+    name: "Cat Fact Generator",
+    id: 1,
+    language: "Python",
+    description: "This is the description This is the description This is the description This is the description This is the description This is the description",
+    takeaway: "Another takeaway",
+  },
+  {
+    name: "Local Weather Data Gatherer",
+    id: 2,
+    language: "Python",
+    description: "This is the description",
+    takeaway: "This is a takeaway",
+  },
+];
 
+// TODO remove image and focus on the content of what I did, what I learned, and what I struggled with.
 const Projects = () => {
   return (
     <div className="projects">
       <div className="project-holder">
         {projects.map((project) => (
           <div className="project" key={project.id}>
-            <div className="title">{`> ${project.name}`}</div>
-            <img className="project-image" src={project.image} alt={project.alt} />
-            <div className="overview">
-                <span className="language">{`${project.language}`}</span>
-                <span className="difficulty">{`${project.difficulty}`}</span>
-
-            </div>
-            <div className="content">
-              <p>{project.content}</p>
-            </div>
+            <div className="project-title">{project.name}</div>
+            <ul>
+              <li className="language">
+                <div>Languages:</div>
+                <li>{project.language}</li>
+              </li>
+              <li className="takeaway">
+                <div>Takeaway:</div>
+                <li>{project.takeaway}</li>
+              </li>
+              <li className="description">
+                <div>Description:</div>
+                <li>{project.description}</li>
+              </li>
+            </ul>
           </div>
         ))}
       </div>
