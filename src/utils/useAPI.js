4969961f078adc1b4713108e1c3b3fcd5e10d423
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-07-19 10:03:46 -0600

    home now gets data from api

---
+++ src/utils/useAPI.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({ baseURL: "//136.36.11.84:3000/" });
+
+export const useAPI = () => {
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      throw error;
+    }
+  );
+  return api;
+};
