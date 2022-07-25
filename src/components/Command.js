33595dc05f9079aec87a77b918c6be8756142564
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-07-25 13:31:18 -0600

    remove comment

--- src/components/Command.js
+++ src/components/Command.js
@@ -1,88 +1,81 @@
-import "./Command.scss";
-import { useState, useEffect } from "react";
-import { isMobile } from "react-device-detect";
-import { changeTheme, executeCommand } from "../utils/execution";
-const commands = require("../utils/commands");
-
-// TODO Possibly use this to check for browser compatibility and fallback
-// if (typeof favDialog.showModal === "function") {
-//   favDialog.showModal();
-// } else {
-//   alert("The <dialog> API is not supported by this browser");
-// }
+import './Command.scss'
+import { useState, useEffect } from 'react'
+import { isMobile } from 'react-device-detect'
+import { changeTheme, executeCommand } from '../utils/execution'
+const commands = require('../utils/commands')
 
 const Command = () => {
-  const [input, setInput] = useState("");
-  const [modalOpen, setModalOpen] = useState(false);
+  const [input, setInput] = useState('')
+  const [modalOpen, setModalOpen] = useState(false)
   const [color, setColor] = useState(
-    () => localStorage.getItem("color") ?? "green"
-  );
-  let valid;
-  let command;
+    () => localStorage.getItem('color') ?? 'green'
+  )
+  let valid
+  let command
 
   useEffect(() => {
-    setColor(setColor, color);
-    changeTheme(setColor, color);
-    localStorage.setItem("color", color);
-  }, [color]);
+    setColor(setColor, color)
+    changeTheme(setColor, color)
+    localStorage.setItem('color', color)
+  }, [color])
 
   useEffect(() => {
-    let modal = document.querySelector(".help-modal");
+    let modal = document.querySelector('.help-modal')
     // Prevents website from breaking on mobile.  Need to figure out modal for mobile
-    if (typeof modal.showModal === "function") {
+    if (typeof modal.showModal === 'function') {
       if (modalOpen) {
-        modal.showModal();
+        modal.showModal()
       } else {
-        modal.close();
+        modal.close()
       }
     } else {
-      console.log("Cannot use dialog");
+      console.log('Cannot use dialog')
     }
-  }, [modalOpen]);
+  }, [modalOpen])
 
   const closeModal = () => {
-    setModalOpen(false);
-  };
+    setModalOpen(false)
+  }
 
   const clearInput = () => {
-    setInput("");
-    document.querySelector(".currentcommand").classList.remove("invalid");
-  };
+    setInput('')
+    document.querySelector('.currentcommand').classList.remove('invalid')
+  }
 
   const handle = () => {
     // Going to probably need regex to test for valid command and arg
     if (valid) {
-      if (command[0] === "help") {
-        setModalOpen(true);
-        clearInput();
-        return;
+      if (command[0] === 'help') {
+        setModalOpen(true)
+        clearInput()
+        return
       }
       // execute command
-      let parameters = input.split(" ").slice(1);
-      executeCommand(setColor, command[0], ...parameters);
-      clearInput();
+      let parameters = input.split(' ').slice(1)
+      executeCommand(setColor, command[0], ...parameters)
+      clearInput()
     } else {
-      setInput(`try typing 'help'`);
-      setTimeout(clearInput, 750);
+      setInput(`try typing 'help'`)
+      setTimeout(clearInput, 750)
     }
-  };
+  }
 
   const checkCommand = () => {
-    command = input.split(" ");
-    valid = commands["Valid"].includes(command[0]) || command[0] === "";
+    command = input.split(' ')
+    valid = commands['Valid'].includes(command[0]) || command[0] === ''
     if (valid) {
-      document.querySelector(".currentcommand").classList.add("valid");
-      document.querySelector(".currentcommand").classList.remove("invalid");
+      document.querySelector('.currentcommand').classList.add('valid')
+      document.querySelector('.currentcommand').classList.remove('invalid')
     } else {
-      document.querySelector(".currentcommand").classList.add("invalid");
+      document.querySelector('.currentcommand').classList.add('invalid')
     }
-  };
+  }
 
-  document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-      setModalOpen(false);
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      setModalOpen(false)
     }
-  });
+  })
 
   return (
     <span>
@@ -90,50 +83,50 @@ const Command = () => {
         autoFocus
         className={`currentcommand valid`}
         onInput={(e) => setInput(e.target.value)}
-        onKeyDown={(e) => e.key === "Enter" && handle()}
+        onKeyDown={(e) => e.key === 'Enter' && handle()}
         onKeyUp={() => checkCommand()}
-        spellCheck="false"
-        type="text"
+        spellCheck='false'
+        type='text'
         value={input}
       />
-      <dialog className="help-modal">
-        <div className="help-container">
+      <dialog className='help-modal'>
+        <div className='help-container'>
           <h1>
-            Welcome to the Help Modal!{" "}
+            Welcome to the Help Modal!{' '}
             <div>
               This modal will show you the commands that can be used in the
               header terminal
             </div>
             <div>
-              Invalid commands will appear <span className="invalid">red</span>.
-              Once the command is recognized it will turn the{" "}
-              <span className="valid">color</span> of the chosen theme
+              Invalid commands will appear <span className='invalid'>red</span>.
+              Once the command is recognized it will turn the{' '}
+              <span className='valid'>color</span> of the chosen theme
             </div>
-            <div className="close-message">
-              Press{" "}
+            <div className='close-message'>
+              Press{' '}
               {isMobile ? (
                 <>
                   -&gt;
-                  <button className="close-modal" onClick={closeModal}>
+                  <button className='close-modal' onClick={closeModal}>
                     HERE
                   </button>
                   &lt;-
                 </>
               ) : (
                 "the 'esc' key"
-              )}{" "}
+              )}{' '}
               to close the help modal when you are ready to give some commands a
               try
             </div>
           </h1>
-          <h2 className="color">
+          <h2 className='color'>
             color &#60;color&#62;
             <div>
               -This will change the primary color theme of the entire
               website/pages-
             </div>
           </h2>
-          <ul className="colors">
+          <ul className='colors'>
             <li>blue</li>
             <li>green</li>
             <li>lightblue</li>
@@ -144,21 +137,21 @@ const Command = () => {
             <li>yellow</li>
             <li>white</li>
           </ul>
-          <h2 className="page">
+          <h2 className='page'>
             cd &#60;page&#62;
             <div>
               -[c]hange [d]irectory will navigate to the given page-
               <div>(home and '.' both take you to the homepage)</div>
             </div>
           </h2>
-          <ul className="pages">
+          <ul className='pages'>
             <li>projects</li>
             <li>contact</li>
             <li>home</li>
             <li>.</li>
           </ul>
-          <h2 className="linkedin">linkedin</h2>
-          <h2 className="github">
+          <h2 className='linkedin'>linkedin</h2>
+          <h2 className='github'>
             github
             <div>
               -This will open a new tab and navigate you to my linkedin/github
@@ -168,7 +161,7 @@ const Command = () => {
         </div>
       </dialog>
     </span>
-  );
-};
+  )
+}
 
-export default Command;
+export default Command
