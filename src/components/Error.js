b8e6bbe0828b2cee36d4825ca13b2267513d6d6e
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-07-27 08:06:30 -0600

    handling errors

--- src/components/Error.js
+++ src/components/Error.js
@@ -6,7 +6,7 @@ const Error = ({ errorMessage }) => {
     <div className="card error">
       <div className="title">Sorry, we ran into an issue</div>
       <div className="content">This is what I could gather about this issue</div>
-      <div className="content">
+      <div className="content error">
         {code}: {message}
       </div>
     </div>
