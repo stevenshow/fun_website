672d18a4042ce9af850ec26fc3faa88980e0d61c
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-07-27 17:08:12 -0600

    styled error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -14,30 +14,36 @@ const Error = ({ errorMessage }) => {
       <div className="title">It seems the website has run into an issue</div>
       <div className="content">This is what I could gather about this issue:</div>
       <div>
-        <li className="content error">
-          {code}: {message}
-        </li>
+        <ul className="error-list">
+          <li className="content error">
+            {code}: {message}
+          </li>
+        </ul>
       </div>
       <div className="content">Likely Cause(s):</div>
-      {errorExists ? (
-        errors[code].Causes.map((cause, i) => (
-          <li className="content cause" key={i}>
-            {cause}
-          </li>
-        ))
-      ) : (
-        <>Cannot find cause</>
-      )}
+      <ul className="error-list">
+        {errorExists ? (
+          errors[code].Causes.map((cause, i) => (
+            <li className="content cause" key={i}>
+              {cause}
+            </li>
+          ))
+        ) : (
+          <>Cannot find cause</>
+        )}
+      </ul>
       <div className="content">Possible Solution(s):</div>
-      {errorExists ? (
-        errors[code].Solution.map((solution, i) => (
-          <li className="content solution" key={i}>
-            {solution}
-          </li>
-        ))
-      ) : (
-        <></>
-      )}
+      <ul className="error-list">
+        {errorExists ? (
+          errors[code].Solution.map((solution, i) => (
+            <li className="content solution" key={i}>
+              {solution}
+            </li>
+          ))
+        ) : (
+          <></>
+        )}
+      </ul>
     </div>
   );
 };
