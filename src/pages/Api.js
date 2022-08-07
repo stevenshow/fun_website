2df9c8e62b9ef2263304ef738d7d62a4900e39cf
stevenshow <steven.schoebinger@gmail.com>
2022-08-07 16:18:41 -0600

    loader for getting data

--- src/pages/Api.js
+++ src/pages/Api.js
@@ -1,25 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useAPI } from '../utils/useAPI';
 import Error from '../components/Error';
 import './Api.scss';
 
 const Api = () => {
   const api = useAPI();
-  const [price, setPrice] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [price, setPrice] = useState(undefined);
   const [error, setError] = useState({ code: undefined, message: undefined });
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const res = await api.get('/products/powerade');
-        console.log(res);
-        setPrice(res.data);
-      } catch (e) {
-        setError({ code: e.code, message: e.message });
-      }
-    };
-    getData();
-  }, [api]);
+  const getPowerade = async () => {
+    try {
+      setPrice(undefined);
+      setLoading(true);
+      const res = await api.get('/products/powerade');
+      console.log(res);
+      setPrice(res.data);
+      setLoading(false);
+    } catch (e) {
+      setError({ code: e.code, message: e.message });
+    }
+  };
 
   return (
     <>
@@ -28,11 +29,25 @@ const Api = () => {
           <div className="card" style={{ minHeight: 'fit-content' }}>
             <div className="title">Powerade Price</div>
             <div className="content" style={{ display: 'flex', flexDirection: 'column' }}>
-              Please click the button to retrieve the prices for powerade at Smiths
-              <div className="location">{price.location}</div>
-              <div className="price">{price.pickup}</div>
-              <button className="get-button" style={{ justifyContent: 'flex-end' }}>
-                Hello
+              Please click the button to retrieve the prices for Mountain Berry Powerade at Smiths
+              {loading && <div className="lds-dual-ring"></div>}
+              {price && (
+                <>
+                  <div className="location" style={{ marginTop: '1rem' }}>
+                    Location: <span>{price.location}</span>
+                  </div>
+                  <div className="price">
+                    Price: <span>{price.pickup}</span>
+                  </div>
+                </>
+              )}
+              {error['code'] != null && <Error errorMessage={error} />}
+              <button
+                className="get-button"
+                onClick={getPowerade}
+                style={{ alignSelf: 'flex-end', marginTop: '1rem' }}
+              >
+                GET
               </button>
             </div>
           </div>
